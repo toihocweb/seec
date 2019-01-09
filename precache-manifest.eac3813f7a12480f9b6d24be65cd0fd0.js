8ccec5b4465862700218b94593b960d4f2423a1a
Nhat <nhat.bui@se-ec.com.vn>
2019-01-09 16:56:48 +0700

    Updates

---
+++ precache-manifest.eac3813f7a12480f9b6d24be65cd0fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e516fe26ca9e99ad7204",
+    "url": "/static/js/main.e516fe26.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e516fe26ca9e99ad7204",
+    "url": "/static/css/main.9294ad61.chunk.css"
+  },
+  {
+    "revision": "acbfecb058d62fc08448b537b7b7ddb1",
+    "url": "/index.html"
+  }
+];
